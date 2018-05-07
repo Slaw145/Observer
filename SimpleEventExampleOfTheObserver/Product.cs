5030b8c13f8f8de0dd66e19aa97c65c5f7de0f43
Sławomir Kowalski <sawekk982@gmail.com>
2018-05-07 06:26:07 +0100

    Update Product.cs

--- SimpleEventExampleOfTheObserver/Product.cs
+++ SimpleEventExampleOfTheObserver/Product.cs
@@ -4,7 +4,34 @@ using System.Text;
 
 namespace ObserverEventsExample
 {
+    delegate void StateChangeHandler(State newState);
+
     class Product
     {
+        private State _state;
+
+        public State MyState
+        {
+            get { return _state; }
+            set
+            {
+                _state = value;
+                _onChange(_state);
+            }
+        }
+
+        private event StateChangeHandler _onChange;
+
+        public event StateChangeHandler OnStateChange
+        {
+            add
+            {
+                _onChange += value;
+            }
+            remove
+            {
+                _onChange -= value;
+            }
+        }
     }
 }
